a7f3760e216026814e14b082c173db8c6a19168d
Evangelos Tzemis <Etzemis@evangelossmbp2.lan>
2016-10-31 17:29:52 +0200

    Install CocoaPods: - Almofire - SwiftyJSON

---
+++ ConnectMe/NetworkingVC.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingVC.swift
+//  ConnectMe
+//
+//  Created by Evangelos Tzemis on 31/10/16.
+//  Copyright © 2016 etzemis. All rights reserved.
+//
+
+import UIKit
+
+class NetworkingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
